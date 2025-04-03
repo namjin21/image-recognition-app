e01a0c03f15c7266a3b46a50c56197d094d83cac
namjin21 <nammjin21@gmail.com>
2025-04-04 00:51:35 +0900

    chore: create PORT constant for Render deployment

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -10,10 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Backend is working!");
-});
-
 const uploadRoute = require("./routes/uploadImageRoute");
 const processRoute = require("./routes/processImageRoute");
 const metadataRoute = require("./routes/metadataRoute");
@@ -22,8 +18,9 @@ app.use("/upload", authenticateUser, uploadRoute);
 app.use("/process", processRoute);
 app.use("/metadata", metadataRoute);
 
+const PORT = process.env.PORT || 3001;
 // if (process.env.IS_OFFLINE) {
-app.listen(3001, () => console.log("Running locally on port 3001"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // }
 
 // Wraps the Express app so that AWS Lambda can handle HTTP requests when triggered via API Gateway.
